a7fad5fa71b135a22e916ccd23fe54bf5d16428f
19042949 <1067470237@qq.com>
2020-01-15 19:16:19 +0800

    修改token刷新评率-jzw

--- src/main/java/com/lelian/mqtt/quartz/JobAutoRunService.java
+++ src/main/java/com/lelian/mqtt/quartz/JobAutoRunService.java
@@ -29,7 +29,7 @@ public class JobAutoRunService implements ApplicationRunner {
         //每10个小时刷新一下token
         schedulerManager.startJob("0 0 0/5 * * ? *","tokenJob","tokenJobGroup", TokenScheduledJob.class);
         //每天凌晨20秒的时候开始运行
-        schedulerManager.startJob("0 0/15 * * * ? *","realTimeJob","realTimeJobGroup", RealTimeScheduledJob.class);
+        schedulerManager.startJob("0 0 0/2 * * ? *","realTimeJob","realTimeJobGroup", RealTimeScheduledJob.class);
     }
 
 }
