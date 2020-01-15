41e9c4af21bcc9e58b12a5579084022b7e6c0a8a
19042949 <1067470237@qq.com>
2020-01-15 21:02:21 +0800

    分次请求-jzw

--- src/main/java/com/lelian/mqtt/service/ServiceApi.java
+++ src/main/java/com/lelian/mqtt/service/ServiceApi.java
@@ -3,6 +3,7 @@ package com.lelian.mqtt.service;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.lelian.mqtt.util.Constant;
 import com.lelian.mqtt.util.HttpUtil;
@@ -18,6 +19,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -79,29 +81,26 @@ public class ServiceApi {
                 deviceSet.add(object.toString().split(",")[0]);
             }
 
-            String deviceIds = "";
+            List<String> idList = Lists.newArrayList();
+            idList.addAll(deviceSet);
 
-            JSONArray jsonArray = new JSONArray();
-            for(String val : deviceSet){
-                jsonArray.add(val);
-            }
-
-            for(int i=0;i<jsonArray.size();i++){
-                if(i == jsonArray.size()-1){
-                    deviceIds = deviceIds + jsonArray.get(i).toString();
+            //分次请求
+            int step = 10;
+            for(int i=0;i<idList.size();){
+                List<String> tempList;
+                if(i + step >= idList.size()){
+                    tempList = idList.subList(i,idList.size() - 1);
+                    dealData(tempList);
+                    break;
                 }else{
-                    deviceIds = deviceIds + jsonArray.get(i).toString() + ",";
+                    tempList = idList.subList(i,i+step - 1);
+                    dealData(tempList);
                 }
+                i = i + step;
             }
-            String link = HOST+REALTIME_API+"?token="+Constant.accessToken+"&hash=test&deviceIds="+deviceIds;
-            String result = HttpUtil.get(link);
-            if(StringUtils.isBlank(result)){
-                return;
-            }
-            JSONObject jsonObject = JSONObject.parseObject(result);
-            slRemoteService.handle(jsonObject);
-        }catch (Exception e){
-            logger.error("getRealTimeData error !",e);
+
+        } catch (IOException e) {
+            logger.error("getRealTimeData error !", e);
         }
     }
 
@@ -114,6 +113,30 @@ public class ServiceApi {
             stringBuilder.append(line);
         }
         return stringBuilder.toString();
+    }
+
+    public void dealData(List<String> dataList) {
+        try {
+            String deviceIds = "";
+            for(int i=0;i<dataList.size();i++){
+                if(i == dataList.size()-1){
+                    deviceIds = deviceIds + dataList.get(i);
+                }else{
+                    deviceIds = deviceIds + dataList.get(i) + ",";
+                }
+            }
+
+            String link = HOST + REALTIME_API + "?token=" + Constant.accessToken + "&hash=test&deviceIds=" + deviceIds;
+            logger.info("请求地址 : "+link);
+            String result = HttpUtil.get(link);
+            if (StringUtils.isBlank(result)) {
+                return;
+            }
+            //JSONObject jsonObject = JSONObject.parseObject(result);
+            //slRemoteService.handle(jsonObject);
+        } catch (Exception e) {
+            logger.error("dealData error !", e);
+        }
     }
 
 }
